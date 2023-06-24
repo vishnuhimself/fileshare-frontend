f8c212582b05900ca3bfc4095f45972b25d5c210
vishnuhimself <dreams.sri@gmail.com>
2023-06-24 19:09:49 +0530

    ..

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import LoginPage from './pages/LoginPage';
+import LoginPage from '../pages/LoginPage';
 
 function Main() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { unstable_createRoot } from 'react-dom';
+import { createRoot } from 'react-dom';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-unstable_createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <React.StrictMode>
       <App />
